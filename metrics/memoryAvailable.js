6b2229789bc3ae06a3aeea176dc23dedd0c3e363
Elad Nava <eladnava@gmail.com>
2024-10-04 10:51:45 -0600

    memoryAvailable.js: Execute "free -m" to get available memory (accounts for buff/cache)

--- metrics/memoryAvailable.js
+++ metrics/memoryAvailable.js
@@ -1,9 +1,29 @@
-var os = require('os');
+var exec = require('child_process').exec;
 
 module.exports = function () {
     // Promise API
     return new Promise(function (resolve, reject) {
-        // Get available system memory (in bytes) and convert to megabytes
-        resolve(Math.round(os.freemem() / 1024 / 1024));
+        // Execute the 'free -m' command to get memory usage in megabytes
+        exec('free -m', function (error, stdout, stderr) {
+            // Error thrown?
+            if (error) {
+                return reject(new Error(`Error executing command: ${error.message}`));
+            }
+
+            // Error output?
+            if (stderr) {
+                return reject(new Error(`Error output: ${stderr}`));
+            }
+
+            // Parse the output to get specific memory details
+            var lines = stdout.split('\n');
+            var memLine = lines[1].split(/\s+/);
+
+            // Available memory is the 6th element in the array
+            var availableMemory = parseInt(memLine[6]);
+
+            // Resolve promise with available memory
+            resolve(availableMemory);
+        });
     });
 }
